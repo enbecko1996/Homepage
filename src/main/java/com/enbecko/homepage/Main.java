9892f725264c3f730702bbec25e016a016a4449d
Niclas Becker <enbecko1996@googlemail.com>
2019-08-25 18:01:52 +0200

    Add CodeLens

--- src/main/java/com/enbecko/homepage/Main.java
+++ src/main/java/com/enbecko/homepage/Main.java
@@ -1,4 +1,5 @@
-
-public static void main(String[] args) {
-    System.out.println("Hello World");
+public class Main {
+    public static void main(String[] tst) {
+        System.out.println("Hello World");
+    }
 }
